a8a6e2552280fa673f77425508fd1a7047896059
Jorge M. Carrillo <jm.carrillo@uniandes.edu.co>
2022-11-05 12:21:08 -0500

    Se completa segundo punto

--- parcial/src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ parcial/src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -10,12 +10,28 @@ import { VehiculoService } from '../vehiculo.service';
 export class VehiculoListComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
+  marcas: Array<String> = [];
+  totales: Array<number> = [];
 
   constructor(private VehiculoService: VehiculoService) { }
 
   getVehiculos(): void {
     this.VehiculoService.getVehiculos().subscribe((vehiculos) => {
       this.vehiculos = vehiculos;
+
+      let j: number = 0;
+      for(let i=0; i<this.vehiculos.length; i++){
+        const marcaRep = this.marcas.find(x => x == this.vehiculos[i].marca);
+        if(marcaRep === undefined){
+          this.marcas[j] = this.vehiculos[i].marca;
+          this.totales[j] = 1;
+          j++;
+        }
+        else{
+          const indice = this.marcas.findIndex(x => x == marcaRep);
+          this.totales[indice] = this.totales[indice] + 1;
+          };
+        }
     });
   }
 
